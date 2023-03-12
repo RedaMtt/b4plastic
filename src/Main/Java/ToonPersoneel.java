f209d784ac9a689bd5f256a72ce1e2ce992f5ed9
TiboMenardo <91217390+TiboMenardo@users.noreply.github.com>
2023-03-12 20:20:25 +0100

    Depency Injection heel dicht bij het einde

--- src/Main/Java/ToonPersoneel.java
+++ src/Main/Java/ToonPersoneel.java
@@ -2,10 +2,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class ToonPersoneel {
     private static List<Persoon> personen;
 
@@ -15,11 +16,14 @@ public class ToonPersoneel {
     }
 
     public static void main(String[] args) {
+        System.out.println("testje");
+
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(PersoneelConfig.class);
 
         for (Persoon p: personen) {
             System.out.println(p.toString());
         }
+
     }
 }
 
